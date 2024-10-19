e80cf065d2e5174447cfed64b2b16e5eb852c43a
Jean Amadeu <jean.amadeu2@gmail.com>
2024-10-19 18:26:22 -0300

    feat: add createCar logic

--- handler/createCarHandle.go
+++ handler/createCarHandle.go
@@ -4,10 +4,27 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jamadeu/api-cars/schemas"
 )
 
 func CreateCarHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST car",
-	})
+	request := CreateCarRequest{}
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	car := schemas.Car{
+		CarModel:     request.CarModel,
+		Manufacturer: request.Manufacturer,
+		Color:        request.Color,
+		Value:        request.Value,
+	}
+	if err := db.Create(&car).Error; err != nil {
+		logger.Errorf("error creating car: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "creating car on database")
+		return
+	}
+	sendSuccess(ctx, "create-car", car)
 }
