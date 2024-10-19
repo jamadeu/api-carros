7e6d95fea4cd50f52ffc3fccd28f6f47f393b17b
Jean Amadeu <jean.amadeu2@gmail.com>
2024-10-19 19:06:38 -0300

    feat: add listCarsHandler logic

--- handler/listCarsHandler.go
+++ handler/listCarsHandler.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jamadeu/api-cars/schemas"
 )
 
 func ListCarsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST car",
-	})
+	cars := []schemas.Car{}
+
+	if err := db.Find(&cars).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error to find cars")
+		return
+	}
+	sendSuccess(ctx, "list-cars", cars)
 }
